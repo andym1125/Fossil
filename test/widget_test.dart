5af2585f9a13bb65a824f947ad136df4d238b421
Aryan-Patel5475 <118410521+Aryan-Patel5475@users.noreply.github.com>
2024-02-06 11:40:42 -0600

    Aryan/cleaning (#51)
    * Widget tests
    
    * Updated Testing
    
    * cleanning
    
    ---------
    
    Co-authored-by: Angel0002 <mtasatmaz@gmail.com>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fossil/login.dart';
 
 import 'package:fossil/main.dart';
+import 'package:fossil/signup.dart';
 
 void main() {
   testWidgets('UI widgets test', (WidgetTester tester) async {
@@ -20,4 +22,64 @@ void main() {
 
     expect(find.widgetWithText(ElevatedButton, 'Sign Up'), findsOneWidget);
   });
+
+  testWidgets('SignUpPage UI', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp( home: SignupPageState()));
+    expect(find.text('Create Account'), findsOneWidget);
+    expect(find.text('Username @Mastodon.social'), findsOneWidget);
+    expect(find.byType(TextField), findsNWidgets(4));
+    expect(find.byType(ElevatedButton), findsOneWidget);
+
+  } );
+
+  testWidgets('Error fot the mismatched passwords during signup', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: SignupPageState()));
+    // Enter incorrect password
+    await tester.enterText(find.byType(TextField).at(0), 'username');
+    await tester.enterText(find.byType(TextField).at(1), 'email@example.com');
+    await tester.enterText(find.byType(TextField).at(2), 'password1');
+    await tester.enterText(find.byType(TextField).at(3), 'password2');
+    await tester.tap(find.text('next'));
+    await tester.pump();
+
+    // check message
+    expect(find.text('Passwords do not match'), findsOneWidget);
+  });
+  
+
+  testWidgets('LoginPage UI', (WidgetTester tester) async {
+    // Build fidger
+    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp( home: LoginPageState()));
+    //find widget
+    expect(find.text('Welcome Back'), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsOneWidget);
+  } );
+
+  testWidgets('Wrong password or username during login', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoginPageState()));
+    // Enter incorrect password
+    await tester.enterText(find.byType(TextField).at(0), 'email@example.com');
+    await tester.enterText(find.byType(TextField).at(1), 'password1');
+    // Error when looking into 
+    //await tester.tap(find.text('LOG IN'));
+    //await tester.pump();
+
+    // check message
+    //expect(find.text('Invalid E-mail address or password.'), findsOneWidget);
+  });
+
+   testWidgets('Username and password fields take in a text', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoginPageState())); 
+    //Enter text in username
+    await tester.enterText(find.byType(TextField).at(0), 'test_username');
+    expect(find.text('test_username'), findsOneWidget);
+
+    //Enter text in password
+    await tester.enterText(find.byType(TextField).at(1), 'test_password');
+    expect(find.text('test_password'), findsOneWidget);
+  });
+
+  
 }
