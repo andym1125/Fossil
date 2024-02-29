5e03cbc390c7a112714f5d13b4c655f4df77d426
SufBit <sufarshad.99@gmail.com>
2024-02-29 12:10:50 -0600

    added timelines & statuses to mastodon mock

--- test/timeline_test.dart
+++ test/timeline_test.dart
@@ -1,5 +1,13 @@
 import 'package:mastodon_api/mastodon_api.dart';
 import 'package:mockito/annotations.dart';
 
+
 @GenerateNiceMocks([MockSpec<TimelinesV1Service>(), MockSpec<StatusesV1Service>()])
 import 'timeline_test.mocks.dart';
+
+void main() {
+
+ 
+
+  // Add more test cases for other methods as needed
+}
